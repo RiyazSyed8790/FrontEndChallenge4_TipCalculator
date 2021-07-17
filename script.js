df88f643357385dfc48bcca472f40c19fcd03272
RiyazSyed8790 <riyazsyed.8790@gmail.com>
2021-07-17 16:13:36 +0530

    input section styling done

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+function warning(){
+    document.getElementById("spam").style.visibility="visible";
+}
+function warning2(){
+    document.getElementById("spam").style.visibility="hidden";
+}
